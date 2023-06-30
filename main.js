3e41c9b4fecdb5822f9ac05d90f4c90a4d4a0c1c
Gustavo Goulart <129804650+gustavo-goulart@users.noreply.github.com>
2023-06-30 15:34:09 -0300

    validação

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form-numero");
 const campoB = document.getElementById("numB");
+let formEValido = false;
 
 function validarNum(numA, numB) {
   const validacao = numA < numB;
@@ -9,5 +10,20 @@ function validarNum(numA, numB) {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  let formEValido = false;
+  const n1 = document.getElementById("numA");
+  const n2 = document.getElementById("numB");
+  const mensagemSucesso = `O número <b>${n2.value}</b> é Maior que o número <b>${n1.value}</b> - Parabéns Sucesso!`;
+
+  formEValido = validarNum(n2.value);
+  if (formEValido) {
+    const containerMensagemSucesso = document.querySelector(".sucess-message");
+    containerMensagemSucesso.innerHTML = mensagemSucesso;
+    containerMensagemSucesso.style.display = "block";
+
+    n1 = "";
+    n2 = "";
+  } else {
+    n1.style.border = "1px solid red";
+    document.querySelector(".error-message").style.display = "block";
+  }
 });
